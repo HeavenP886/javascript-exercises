ac16cae3404f60a25ee565bb6857a498d1884525
HeavenP886 <53100342+HeavenP886@users.noreply.github.com>
2023-07-10 10:34:42 -0400

    Start of exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num){
+    let heyString = ``;
+    if (num >= 0){
+        for (let i = 0; i < num; i++){
+            heyString += string
+        }
+    } else {
+        return `ERROR`;
+    }
+    return heyString;
+}
 
-};
+
+console.log(repeatString(`hey`,0))
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,num1,num2) {
+    let secondArray = array.splice(num1 -1,1);
+    return array;
 
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
